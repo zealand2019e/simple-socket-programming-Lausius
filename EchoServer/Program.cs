7a497d7aecd4a51df47d5892c936973ab859afcb
Lausius <skinkens_email@yahoo.dk>
2020-09-04 10:24:30 +0200

    refactored server

--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -20,28 +20,36 @@ namespace EchoServer
         public static void Start()
         {
             TcpListener serverSocket = new TcpListener(IPAddress.Loopback, 7);
-
+            serverSocket.Start();
             TcpClient connectionSocket = serverSocket.AcceptTcpClient();
             Console.WriteLine("Server activated");
+            DoClient(connectionSocket);
 
-            Stream ns = connectionSocket.GetStream();
-            StreamReader sr = new StreamReader(ns);
-            StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true;
+            //ns.Close();
+            //Console.WriteLine("net stream closed");
+            //connectionSocket.Close();
+            //Console.WriteLine("connection socket closed");
+            //serverSocket.Stop();
+            //Console.WriteLine("server stopped");
+        }
 
-            string message = sr.ReadLine();
-            Console.WriteLine("received message: " + message);
-            if (message != null)
+        public static void DoClient(TcpClient socket)
+        {
+            using (socket)
             {
-                sw.WriteLine(message.ToUpper());
-            }
+                Stream ns = socket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
 
-            ns.Close();
-            Console.WriteLine("net stream closed");
-            connectionSocket.Close();
-            Console.WriteLine("connection socket closed");
-            serverSocket.Stop();
-            Console.WriteLine("server stopped");
+                string message = sr.ReadLine();
+                Console.WriteLine("received message: " + message);
+                if (message != null)
+                {
+                    sw.WriteLine(message.ToUpper());
+                }
+
+            }
         }
     }
 
